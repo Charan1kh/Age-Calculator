ce026c1f025b9131799596abb6808355080842a0
charan1kh@gmail.com <charan1kh@gmail.com>
2022-02-12 22:59:51 +0530

    column created

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-
-
 import datetime
 import tkinter as tk
 from PIL import Image, ImageTk
 window = tk.Tk()
+window.geometry("540x720")
+window.title("Age Calc")
+name = tk.Label(text = "Name")
+name.grid(column=0,row=1)
+
+window.mainloop()
