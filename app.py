092dc9550f876b3b37f0cebc4f1af7750629e616
charan1kh@gmail.com <charan1kh@gmail.com>
2022-02-13 12:47:16 +0530

    adaptive Reso.

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import datetime
 import tkinter as tk
 from PIL import Image, ImageTk
 
-#window
+#windowRes
 window = tk.Tk()
-window.geometry("540x720")
+window.geometry("440x520")
 window.title("Age Calc")
 
-#fields
+#attributes
 name = tk.Label(text = "Name")
 name.grid(column=0,row=1)
 
@@ -20,7 +20,7 @@ name.grid(column=0,row=3)
 name = tk.Label(text = "Day")
 name.grid(column=0,row=4)
 
-#Entries
+#Inputs
 nameEntry = tk.Entry()
 nameEntry.grid(column=1,row=1)
 
@@ -32,6 +32,19 @@ monthEntry.grid(column=1,row=3)
 
 dateEntry = tk.Entry()
 dateEntry.grid(column=1,row=4)
+
+#fun
+def getInput():
+    name=nameEntry.get()
+    user = Person(name,datetime.date(int(yearEntry.get()),
+    int(monthEntry.get()),
+    int(dateEntry.get())))
+
+    textArea = tk.Text(master=window,height=15,width=25)
+    textArea.gird(column=1, row=6)
+    reply = "Hello {user}!, You're {age} years old.".format(user=name, age=user.age())
+    textArea.insert(tk.End, reply)
+
 
 
 
